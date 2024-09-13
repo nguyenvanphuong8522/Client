0f49d6a9b82d814a722c81ac63016d943edef343
Nguyen Van Phuong <nguyenvanphuong8522@gmail.com>
2024-09-13 18:47:54 +0700

    refactor: byte

--- Assets/Scripts/Socket/SocketManager.cs
+++ Assets/Scripts/Socket/SocketManager.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using MyLibrary;
 using UnityEngine;
+using Unity.VisualScripting;
+using Newtonsoft.Json.Linq;
+using System.Linq;
 
 public class SocketManager :MonoBehaviour
 {
@@ -18,8 +21,45 @@ public class SocketManager :MonoBehaviour
         ipEndPoint = new IPEndPoint(IPAddress.Parse("192.168.1.25"), 8522);
         socket = new Socket(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
         await socket.ConnectAsync(ipEndPoint);
-        string content = JsonConvert.SerializeObject(new MessagePosition());
-        SendMessageToServer(MyUtility.ConvertToDataRequestJson(content, MyMessageType.CREATE));
+        //string content = JsonConvert.SerializeObject(new MessagePosition());
+        //byte[] mainData = MyUtility.ConvertToDataRequestByte(content, MyMessageType.CREATE);
+        //int length = mainData.Length;
+        //byte[] byteLenght = BitConverter.GetBytes(length);
+        //byte[] combinedArray = byteLenght.Concat(mainData).ToArray();
+        
+
+
+
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SendMessageToServer();
+        }
+    }
+    public void SendMessageToServer()
+    {
+        byte[] byteLength1 = new byte[10];
+        byte[] intBytes1 = BitConverter.GetBytes(60);
+        Buffer.BlockCopy(intBytes1, 0, byteLength1, 0, intBytes1.Length);
+
+        byte[] byteLength2 = new byte[20];
+        byte[] intBytes2 = BitConverter.GetBytes(60);
+        Buffer.BlockCopy(intBytes2, 0, byteLength2, 0, intBytes2.Length);
+
+        // Tạo mảng byte kết hợp
+        byte[] combinedArray = new byte[byteLength1.Length + byteLength2.Length];
+
+        // Sao chép mảng byteLength1 vào combinedArray
+        Buffer.BlockCopy(byteLength1, 0, combinedArray, 0, byteLength1.Length);
+
+        // Sao chép mảng byteLength2 vào combinedArray, bắt đầu từ vị trí tiếp theo sau byteLength1
+        Buffer.BlockCopy(byteLength2, 0, combinedArray, byteLength1.Length, byteLength2.Length);
+
+        // In kích thước mảng để kiểm tra
+        Debug.Log($"combinedArray.Length: {combinedArray.Length}");
+        SendMessageToServer(combinedArray);
     }
 
     public async Task WaitReceiveRequest()
